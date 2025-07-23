7d2670b0332619e66acc585f7f4481e78b0cc800
l.amb <85984068+l-amb@users.noreply.github.com>
2025-07-23 20:47:01 +1000

    Create Updatelog.lua

---
+++ Updatelog.lua
@@ -0,0 +1,108 @@
+local updates = {}
+
+updates["v0.4"] = {
+	"!The quest update, we have added a brand new quest system to the game. The main focus of Retro City is to complete every quest given by npcs, but to do that there actually needs to be quests.",
+	"Added a quest system with a screen gui.",
+	--"Added a tutorial quest with a total reward of $30.",
+	"Added 3 fishing quests, talk to Fisherwoman.",
+	--"Added the ability to apply minerals to your fishing rod.",
+	"Made fishing harder to fail but harder to get better fish."
+}
+
+updates["v0.3b"] = {
+	"!The corner store has been released. This mini update releases the corner store with many new items to obtain daily.",
+	"Added 5 new items to the game.",
+	"Added a sell box, sell your items for cheap.",
+	"Added more housing.",
+	"Added shiny jewel seller.",
+	"Added the ability to dig dirt patches.",
+	"Added digging quest npc 'Tony'. - Quests next update 👀",
+	"Added some secret badges.",
+	"Added daily disco event, can also be triggered manually.",
+	"Added aurora weather - 1 in 2,022 chance every 40 seconds.",
+	"Extended some roads.",
+	"Fixed 1 bugs.",
+	"!Hotfixes:",
+	"Fixed fisherwoman acting as Tony & Tony not doing anything.",
+	"Updated Business Man",
+	"Expanded some roads",
+	"Fixed 3 other bugs"
+}
+
+updates["v0.3"] = {
+	"!The corner store has been released. This mini update releases the corner store with many new items to obtain daily.",
+	"Added 5 new items to the game.",
+	"Added a sell box, sell your items for cheap.",
+	"Added more housing.",
+	"Added shiny jewel seller.",
+	"Added the ability to dig dirt patches.",
+	"Added digging quest npc 'Tony'. - Quests next update 👀",
+	"Added some secret badges.",
+	"Added daily disco event, can also be triggered manually.",
+	"Added aurora weather - 1 in 2,022 chance every 40 seconds.",
+	"Extended some roads.",
+	"Fixed 1 bugs."
+}
+
+updates["v0.2b"] = {
+	"!The shop update, in this update we have mainly been working on the shopping experience so that's why we have updated the market, added more to the merchant and dealer and we also are opening a new corner store soon!",
+	"Added super market interior.",
+	"Added a corner store.",
+	"Added more items to Merchant & Dealer",
+	"Fixed Dealer not opening correct shop",
+	"Sell prices now vary depending on the weather.",
+	"Added another house and car.",
+	"Updated road near police station.",
+	"Added a path to the pond.",
+	"Added lightning effects to storms.",
+	"Added 4 new items to the game.",
+	"Added new admin events.",
+	"Updated previous admin events.",
+	"Admin events can now happen globally.",
+	"!PS - The corner store opening will be a live event.",
+	"Updated the water",
+	"Fixed a minor visual bug",
+	"Added shiny jewel seller (v0.3 Early)"
+}
+
+updates["v0.2"] = {
+	"!The shop update, in this update we have mainly been working on the shopping experience so that's why we have updated the market, added more to the merchant and dealer and we also are opening a new corner store soon!",
+	"Added super market interior.",
+	"Added a corner store.",
+	"Added more items to Merchant & Dealer",
+	"Fixed Dealer not opening correct shop",
+	"Sell prices now vary depending on the weather.",
+	"Added another house and car.",
+	"Updated road near police station.",
+	"Added a path to the pond.",
+	"Added lightning effects to storms.",
+	"Added 4 new items to the game.",
+	"Added new admin events.",
+	"Updated previous admin events.",
+	"Admin events can now happen globally.",
+	"!PS - The corner store opening will be a live event."
+}
+
+updates["v0.1b"] = {
+	"Added fruit stand where you can berries",
+	"Harvested berries no longer instantly sell",
+	"Fixed NPCs being stuck in the pond",
+	"Gave the policeman a randomized name",
+	"Updated the main menu",
+	"Added custom leaderboard"
+}
+
+updates["v0.1"] = {
+	"Increased 'Him' time",
+	"Decreased berry base sell price from '6.43' to '2.43'",
+	"Decreased fishing cooldown from '24' to '10'",
+	"Increased berry harvest cooldown from '12' to '20'",
+	"Increased berry hold time from '2' to '4'",
+	"Changed fishing rod design",
+	"Fixed camera glitch when dying",
+	"Made fall damage less sensitive"
+}
+
+updates.latest = updates["v0.3b"]
+
+return updates
