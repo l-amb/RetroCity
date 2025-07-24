794bbc1faeeba67f3ed40290d71260c414e2ef31
l.amb <85984068+l-amb@users.noreply.github.com>
2025-07-24 16:39:34 +1000

    Create Names.lua

---
+++ Names.lua
@@ -0,0 +1,27 @@
+--These names are for automatically generated characters such as the policeman.
+--When contributing please do not add names that characters in the game already have.
+
+local names = {
+	male = {
+		"Bob",
+		"James",
+		"Harry",
+		"Thomas",
+		"Edward",
+		"Jake",
+		"Liam",
+		"Noah",
+		"Lucas",
+		"Theodore",
+		"Henry",
+		"Asher",
+		"Riley",
+		"Quin",
+		"Jayden"
+	},
+  female = {
+    
+  }
+}
+
+return names
